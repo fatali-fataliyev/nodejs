a2164876091168b9e51d2af47fa347575d5c81cb
fatali-fataliyev <fatali.fataliiyev@gmail.com>
2025-01-11 19:04:08 +0400

    working with 3rd party  modules

---
+++ requests/index.js
@@ -0,0 +1,16 @@
+//1. gitignore = using for ignore writed files when pushing.
+
+//2. package.json = use for about prooject and install dependencies
+
+//3. package-lock.json = use for install dependecy of dependencies
+
+const axios = require("axios")
+//1. Check in built-in
+//2. Search in node modules
+//3. Search in Global
+//4. module not found.
+
+
+console.log(axios)
+
+
